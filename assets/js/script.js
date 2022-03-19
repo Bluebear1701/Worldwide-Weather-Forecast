f94d369087b07bb1c11197b8c92c22243260fb1d
Blue <97713796+Bluebear1701@users.noreply.github.com>
2022-03-19 18:58:01 -0500

    get coordinates function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,36 @@
 var appId ="e9c5575e3a5b6939670c3a1588bc2124";
 var cityName;
-var searchButton = document.queryselctor('.btn');
+var searchButton = document.querySelector('.btn');
 var cityInput = document.querySelector("#city");
 
+var getcoordinates = function() {
+fetch(
+    "https://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&appid=" + appId)
+    .then(function(response){
+    return response.json();
+    }).then (function(data){
+        console.log(data)
+    });
+};
+
 var getWeather = function () {
-    fetch
-    console.log(cityInput.value);
+    fetch(
+        "https://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&appid=" + appId
+    )
+    .then (function (response){
+        return response.json();
+    })
+    .then (function(data) {
+       var latitude = data[0].lat;
+        var longitude = data[0].lon; 
+        return [latitude, longitude];
+    })
+    .then (function(value){
+        fetch ("https://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&appid=" + appId)
+        .then(function(response) {
+            return response.json();
+        });
+    });
 };
 
 var citySearch = function() {
