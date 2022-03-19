6a6c2ad95bae3e1c67468923caf6f33e9250941a
Blue <97713796+Bluebear1701@users.noreply.github.com>
2022-03-19 17:19:24 -0500

    add html and got city name

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+var appId ="e9c5575e3a5b6939670c3a1588bc2124";
+var cityName;
+var searchButton = document.queryselctor('.btn');
+var cityInput = document.querySelector("#city");
+
+var getWeather = function () {
+    fetch
+    console.log(cityInput.value);
+};
+
+var citySearch = function() {
+    cityName =cityInput.value;
+}
+searchButton.addEventListener("click", citySearch);
